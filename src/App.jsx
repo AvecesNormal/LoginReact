573b2f082e29366adeb402f403322b0fe77f936e
javier <javierpardollanes@gmail.com>
2024-08-12 21:47:20 -0500

    Correccion en el inicio de sesion que mostraba "usuario no autenticado" desde el primer ingreso a la pagina.

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,56 @@
-import { useState } from 'react' 
-import './App.css'
-import Conversor from './Conversor.jsx'
+import { useEffect, useState } from 'react';
+import './App.css';
+import Conversor from './Conversor.jsx';
 
 function App() {
-  const [usuario, setUsuario] = useState('')
-  const [clave, setClave] = useState('')
-
-  const [logueado, setLogueado] = useState(false)
+  const [usuario, setUsuario] = useState('');
+  const [clave, setClave] = useState('');
+  const [logueado, setLogueado] = useState(false);
 
   function cambiarUsuario(evento) {
-    setUsuario(evento.target.value)
+    setUsuario(evento.target.value);
   }
 
   function cambiarClave(evento) {
-    setClave(evento.target.value)
+    setClave(evento.target.value);
   }
 
-  function Ingresar() {
-    if (usuario === 'admin' && clave === 'admin') {
-      setLogueado(true)
-      alert('Bienvenido')
+  async function Ingresar() {
+    const peticion = await fetch(`http://localhost:3000/login?usuario=${usuario}&clave=${clave}`, {
+      credentials: 'include',
+    });
+    if (peticion.ok) {
+      setLogueado(true);
     } else {
-      alert('Usuario o contraseña incorrectos')
+      alert('Usuario o contraseña incorrectos');
     }
   }
 
+  async function validar() {
+    const peticion = await fetch('http://localhost:3000/validar', { credentials: 'include' });
+    if (peticion.ok) {
+      setLogueado(true);
+    } else {
+      setLogueado(false);
+    }
+  }
+
+  useEffect(() => {
+    validar();
+  }, []);
+
   if (logueado) {
-        return <Conversor />
-      }
+    return <Conversor />;
+  }
 
   return (
-        <>
-          <h1>Inicio de sesión</h1>
-          <input placeholder='Usuario' type="text" name="usuario" id="usuario" value={usuario} onChange={cambiarUsuario} />
-          <input placeholder='Clave' type="password" name="clave" id="clave" value={clave} onChange={cambiarClave} />
-          <button onClick={Ingresar}>Ingresar</button>
-        </>
-  )
+    <>
+      <h1>Inicio de sesión</h1>
+      <input placeholder='Usuario' type="text" name="usuario" id="usuario" value={usuario} onChange={cambiarUsuario} />
+      <input placeholder='Clave' type="password" name="clave" id="clave" value={clave} onChange={cambiarClave} />
+      <button onClick={Ingresar}>Ingresar</button>
+    </>
+  );
 }
 
-export default App
+export default App;
